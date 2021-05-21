7d00e6b156c13254cba1785ace3f0b9f0f9c3147
vinszip999 <w2011@e-mirim.hs.kr>
2021-05-21 09:50:09 +0900

    문제5번 완성

--- 문제05-보조.py
+++ 문제05-보조.py
@@ -1,17 +1,18 @@
 # 숫자를 입력하고 *1부터 입력받은 숫자까지*
 
+def solution(number):
+   count = 0
+   for i in range(1, number + 1):
+       current = i
+       temp = count
+       while current != 0:
+           if current % 10 == 3 or current % 10 == 6 or current % 10 == 9:
+           # if current % 10 in [3, 6, 9]:
+               count += 1
+           current = current // 10
+   return count
 
-# 방법 2 : 컴퓨터가 생각하는 방법
-a = 33
-count = 0
-
-for x in range(1, a+1):
-    while x:
-        # if a % 10 == 3 or a % 10 == 6 or a % 10 == 9:
-        if x % 10 in [3, 6, 9]:
-            count += 1
-        x = x // 10 # 소수점 자르기 //
-
-print(count)
-
-
+# The following is code to output testcase.
+number = 40
+ret = solution(number)
+print(ret)
