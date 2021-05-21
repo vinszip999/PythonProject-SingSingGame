d3ee4ff61884fb3c119643df7fc0592a8a1853e9
vinszip999 <w2011@e-mirim.hs.kr>
2021-05-21 09:03:08 +0900

    문제5번 보조1

---
+++ 문제05-보조.py
@@ -0,0 +1,17 @@
+# 숫자를 입력하고 입력받은 숫자가 몇 번 박수를 치는지 알아보자
+
+# 방법 1 : 사람이 생각하는 방법
+a = 33
+# # print(type(a))
+문자열 = str(a)
+# # print(type(문자열)
+# type를 확인하는 방법 type(변수명)
+
+count = 0
+for x in 문자열:
+    if x == '3' or x == '6' or x == '9':
+        # 카운트를 증가시킨다
+        count += 1
+print(count)
+
+
