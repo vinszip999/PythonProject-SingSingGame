2dc7bd70f398ba220da26336cca2ebd553a5c047
vinszip999 <w2011@e-mirim.hs.kr>
2021-06-11 09:12:38 +0900

    Lv1-01 음양더하기 완성

--- 프로그래머스Lv1/01 음양더하기.py
+++ 프로그래머스Lv1/01 음양더하기.py
@@ -1,7 +1,15 @@
 def solution(absolutes, signs):
-    answer = 123456789
+    answer = 0
+    for i in range(len(absolutes)):
+        # True 에 해당하는 요소(item)는 더한다
+        if signs[i] is True: # 참과 거짓을 비교할 땐 == 보단 is를 많이 쓴다
+            answer += absolutes[i]
+        # False 에 해당하는 요소(item)는 뺀다
+        else:
+            answer -= absolutes[i]
     return answer
 
-a = [4, 7, 12]
-s = [True, False, True]
-print(solution(a, s))
+if __name__ == "__main__":
+    a = [4, 7, 12]
+    s = [True, False, True]
+    print(solution(a, s))
