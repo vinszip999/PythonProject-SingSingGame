f13d6328addb2f593048f17b20f03345ab99725c
vinszip999 <w2011@e-mirim.hs.kr>
2021-08-30 12:08:30 +0900

    package 2/2

--- module, package/do.py
+++ module, package/do.py
@@ -1,2 +1,25 @@
-import my_module
-my_module.nara_friend()
+# import my_module
+# my_module.nara_friend()
+# import tour.jeonnam
+# tour.jeonnam.채리()
+#
+# from tour import jeonnam
+# jeonnam.채리()
+#
+# from tour.jeonnam import 채리
+# 채리()
+
+# import tour.jeonnam as tj
+# tj.채리()
+#
+# from tour import jeonnam as je
+# je.채리()
+#
+# from tour.jeonnam import 채리 as cherry
+# cherry()
+
+from tour import *
+jeonnam.채리()
+
+gp = gangwondo.GangwondoPackage()
+print(gp)
