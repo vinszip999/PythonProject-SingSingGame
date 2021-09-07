ec10346fc39bff2ba7633d5393d6ab7aa7a4b0a7
vinszip999 <w2011@e-mirim.hs.kr>
2021-09-07 21:32:05 +0900

    1학기 Python Project - StudyCafe Completed version

--- studycafe/cafe.py
+++ studycafe/cafe.py
@@ -16,7 +16,7 @@ class Cafe:
     def time_choice(self):
         while True:
             pay = 2000
-            print('=====================================================================')
+            print('='*60)
             time = int(input("| 1시간 - 2000원 | 2시간 - 4000원 | 3시간 - 6000원 | 4시간 - 8000원 |\n"
                              "| 5시간 - 10000원 | 6시간 - 12000원 | 7시간 - 14000원 | 8시간 - 16000원 |\n"
                              ">> 1:00(최소) ~ 8:00(최대) 원하시는 시간을 고르세요 ex) 1 : "))
@@ -24,15 +24,15 @@ class Cafe:
                 self.sum = pay * time
                 break
             else:
-                print('--------------------------------\n'
-                      '*** 1~8 사이의 숫자만 입력하세요 ***')
+                print('-'*30)
+                print('\n*** 1~8 사이의 숫자만 입력하세요 ***')
 
     def checkin(self):
         while True:
-            print('=====================================================================')
+            print('='*60)
             print('<입실 가능한 좌석>')
             self.show_empty_seat_list()  # 빈자리 보여주기
-            print('-------------------------------')
+            print('-'*30)
             self.checkin_position = input('1~10까지 입실 가능한 좌석 중 사용할 좌석을 선택하세요 ex) 1 : ')  # 자리선택하기
             self.checkin_position = int(self.checkin_position)
             new_checkin = Checkin()  # Checkin 생성하기
@@ -44,14 +44,14 @@ class Cafe:
                         self.check_list.append(self.checkin_position)
                         self.empty_seat_list.remove(self.checkin_position)
                         return
-                    print('-------------------------------------/n'
-                          '*** 1~10까지 좌석 중에서 선택해주세요 ***')
-            print('---------------------------------------------\n'
-                  '*** 입실 가능한 좌석 중 다시 골라주세요 ***')
+                    print('-'*30)
+                    print('\n*** 1~10까지 좌석 중에서 선택해주세요 ***')
+            print('-'*40)
+            print('\n*** 입실 가능한 좌석 중 다시 골라주세요 ***')
 
     # 주문 내역 보여주기
     def order_details(self):
-        print('=====================================================================')
+        print('='*60)
         print(f'고객님의 좌석은 {self.checkin_position}번 입니다.\n'
               f'결제할 금액은 "{self.sum}"원 입니다.\n'
               f'고객님의 보안번호는 "{self.seat_list[self.checkin_position - 1].checkin.random_number}" 입니다.\n'
@@ -60,15 +60,15 @@ class Cafe:
     # 결제하기
     def payment(self):
         while True:
-            print('=====================================================================')
+            print('='*60)
             pay = input('결제 금액을 입력하세요 : ')
             pay = int(pay)
             if pay == self.sum:
                 print('결제가 완료되었습니다~ 공부 열심히 하세요~')
                 break
             else:
-                print('----------------------------------------------\n'
-                      '*** 주문 내역에 있는 결제 금액을 다시 입력하세요 ***')
+                print('-'*40)
+                print('\n*** 주문 내역에 있는 결제 금액을 다시 입력하세요 ***')
 
     # 빈자리 보여주기
     def show_empty_seat_list(self):
@@ -81,11 +81,11 @@ class Cafe:
     # 퇴실하기
     def checkout(self):
         if not self.check_list:
-            print('-------------------\n'
-                  '*** 퇴실 가능한 좌석이 없습니다. ***')
+            print('-'*20)
+            print('\n*** 퇴실 가능한 좌석이 없습니다. ***')
         else:
             while True:
-                print('=====================================================================')
+                print('='*40)
                 print('<사용중인 좌석>')
                 print(sorted(self.check_list))
                 print('--------------------')
@@ -94,7 +94,7 @@ class Cafe:
                 if 1 <= checkout_position <= 10:
                     if checkout_position not in self.empty_seat_list:  # [1,2,3,4,5,6,7,8,9,10]
                         while True:
-                            print('=====================================================================')
+                            print('='*60)
                             rn = input('입실시 받은 보안번호는: ')
                             rn = int(rn)
                             if self.seat_list[checkout_position - 1].checkin.random_number == rn:
@@ -104,11 +104,12 @@ class Cafe:
                                 print('퇴실이 성공적으로 완료되었습니다. 안녕히 가십시오~')
                                 return
                             else:
-                                print('----------------------------------------\n'
-                                      '*** 번호가 틀리셨습니다 재입력 해주십시오 ***')
+                                print('-'*40)
+                                print('\n*** 번호가 틀리셨습니다 재입력 해주십시오 ***')
                     else:
-                        print('-------------------------------------\n'
-                              '--- 퇴실 가능한 좌석이 아닙니다. ---\n'
+                        print('-'*40)
+                        print('\n')
+                        print('--- 퇴실 가능한 좌석이 아닙니다. ---\n'
                               '*** 입실하신 좌석 중 다시 입력하세요 ***')
                         break
                 else:
