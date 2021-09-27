feb4a1ba41a6c7e19589ebc7133d417758ed4ed1
vinszip999 <w2011@e-mirim.hs.kr>
2021-09-27 14:11:08 +0900

    save text(utf-8), '\n'

--- file handler/save_text.py
+++ file handler/save_text.py
@@ -1,8 +1,9 @@
-f = open('text.txt', 'w')
+f = open('text.txt', 'w', encoding='utf-8')
 
-f.write('이유빈')
+f.write('이유빈 - ')
 f.write('초록색')
-f.write('김효진')
+f.write('\n')
+f.write('김효진 - ')
 f.write('하늘색')
 
 f.close()
