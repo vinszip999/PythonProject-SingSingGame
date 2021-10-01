d9ad6266056fbdcef48be30a462fb153087a6b92
vinszip999 <w2011@e-mirim.hs.kr>
2021-10-01 15:37:22 +0900

    save text, with

--- file handler/save_text.py
+++ file handler/save_text.py
@@ -7,3 +7,11 @@ f.write('김효진 - ')
 f.write('하늘색')
 
 f.close()
+
+with open('text.txt', 'w', encoding='utf-8') as f:
+    f.write('이유빈 - ')
+    f.write('초록색')
+    f.write('\n')
+    f.write('김효진 - ')
+    f.write('하늘색')
+
