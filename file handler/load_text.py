9f58d6737ef6b3b19087cf304ab825280af920b7
vinszip999 <w2011@e-mirim.hs.kr>
2021-10-01 15:31:42 +0900

    ** load text, readline, split **

--- file handler/load_text.py
+++ file handler/load_text.py
@@ -5,7 +5,7 @@ data = f.read()
 f.close()
 print(data)
 
-print('<한 줄식 읽기>')
+print('<한 줄식 읽기>')  # 제일 많이 쓰이는 방법
 
 f = open('text.txt', 'r', encoding='utf-8')
 while True:
@@ -16,11 +16,29 @@ while True:
 
 f.close()
 
-print('<전체를 한꺼번에 읽고, 줄별로 리스트>')
+print('<전체를 한꺼번에 읽고, 줄별로 리스트>')  # 두 번째로 많이 쓰이는 방법
 f = open('text.txt', 'r', encoding='utf-8')
 lines = f.readlines()
 f.close()
 for line in lines:
     print(line.rstrip())
+
+
+# quiz
+# 이름 : 이유빈[tab]좋아하는 색: 초록색
+# 이름 : 김효진[tab]좋아하는 색: 하늘색
+
+print('이름: ')
+print('좋아하는 색: ')
+
+print('<전체를 한꺼번에 읽고, 줄별로 리스트>')  # 두 번째로 많이 쓰이는 방법
+f = open('text.txt', 'r', encoding='utf-8')
+lines = f.readlines()
+f.close()
+for line in lines:
+    # line = '프리:노랑색'
+    data = line.split(':')  # ['프리', '노랑색']
+    # print('이름: ', line.rstrip()[:3]+"\t좋아하는 색: "+line.rstrip()[6:])
+    print('이름: ', data[0].rstrip()+"\t좋아하는 색: "+data[1].rstrip())
 
 
