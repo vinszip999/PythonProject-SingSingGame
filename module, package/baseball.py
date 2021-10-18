a8ff861a4254d6e176bbb6e56e4515d80ca730b2
vinszip999 <w2011@e-mirim.hs.kr>
2021-10-18 14:10:14 +0900

    count, name save, load

--- module, package/baseball.py
+++ module, package/baseball.py
@@ -1,16 +1,48 @@
 from baseball_game_engine import make_answer, check
 # from custom_error import InvalidLengthError
+
+def save_history(answer, count, name):
+   with open('baseball_history.txt', 'a', encoding='utf-8') as f:
+       f.write(f'{answer}:{count}:{name}\n')
+
+def load_history():
+    count_name = {}
+    with open('baseball_history.txt', 'r', encoding='utf-8') as f:
+        print('----history----')
+        while True:
+            line = f.readline()  # 한줄 읽기
+            if line == '':  # 파일 끝이면 끝내기
+                break
+            print(line.rstrip())  # rstrip() - 내려쓰기 지워짐  ('\n' 지우기)
+            line = line.rstrip()  # answer:count:name
+            data = line.split(':')
+            count_name[data[1]] = data[2]
+        #top3
+        count_name = sorted(count_name.items())  # 정렬하기
+        return count_name[:3]  # top3
+
+
+
 answer = make_answer()
-# print(answer)
+print(answer)
 
 # number = random.sample(range(0, 9), 3)
 # print(str(number[0]) + str(number[1]) + str(number[2]))
 
 count = 0
 # 무한반복
+
+
+
+
 while True:
     # 숫자 묻자
-    guess = input("숫자를 입력하세요 : ")
+    guess = input("숫자를 입력하세요 (t: history, top3) : ")
+    # t를 입력하면, 불러오자, top3
+    if guess == 't':
+        top3 = load_history()
+        print(top3)
+        continue
     # 숫자인지 아닌지 확인하자
     try:
         guess_int = int(guess)
@@ -31,4 +63,11 @@ while True:
     # 정답 == 숫자, 끝내기
     if answer == guess:
         print('정답입니다!!')
+        # 저장하자, 정답, 시도횟수
+        name = input('이름은 : ')
+        save_history(answer, count, name)
+
+        # 불러오자, top3
+        top3 = load_history()
+        print(top3)
         break
