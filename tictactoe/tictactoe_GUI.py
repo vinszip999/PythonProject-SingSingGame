2f0dda62edb471f8f07bfd28ba08a4e6d927846f
vinszip999 <w2011@e-mirim.hs.kr>
2021-11-05 16:05:52 +0900

    tictactoe GUI __init__

---
+++ tictactoe/tictactoe_GUI.py
@@ -0,0 +1,17 @@
+from tictactoe_game_engine import TictactoeGameEngine
+
+class TictactoeGUI:
+
+    def __init__(self):
+        self.game_engine = TictactoeGameEngine()
+
+    def init_GUI(self):
+        pass
+
+    def click_handler(self, event):
+        pass
+
+
+if __name__ == '__main__':
+    ttt_GUI = TictactoeGUI()
+
