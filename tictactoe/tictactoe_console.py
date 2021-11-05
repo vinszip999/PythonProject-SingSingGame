0142b15076d106a36c3e08c4d799d26e01bc2ad8
vinszip999 <w2011@e-mirim.hs.kr>
2021-11-05 15:35:56 +0900

    tictactoe console 완성

--- tictactoe/tictactoe_console.py
+++ tictactoe/tictactoe_console.py
@@ -1,3 +1,38 @@
+from tictactoe_game_engine import TictactoeGameEngine
+
+class TictactoeConsole:
+
+    def __init__(self):
+        self.game_engine = TictactoeGameEngine()
+
+    def play(self):
+        # show board
+        self.game_engine.show_board()
+        # 무한반복
+        while True:
+            # input, row, col
+            row = int(input('행: '))
+            col = int(input('열: '))
+            # set row, col
+            self.game_engine.set(row, col)
+            # show board
+            self.game_engine.show_board()
+            # set winner
+            winner = self.game_engine.set_winner()
+            # 승자가 있거나 무승부면, 게임 오버, 결과 출력
+            if winner == 'X' or winner == 'O':
+                print(f'{winner} 이김!! 추카추카~ ')
+                break
+            elif winner == 'd':
+                print('무승부!!')
+                break
+            # change turn
+            self.game_engine.change_turn()
+
+if __name__ == '__main__':
+    ttt_console = TictactoeConsole()
+    ttt_console.play()
+
 # show board
 # 무한반복
 #  input row, col
