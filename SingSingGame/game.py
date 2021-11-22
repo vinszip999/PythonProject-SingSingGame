12b114ad74085238f990b4aae68d98fec0142799
vinszip999 <w2011@e-mirim.hs.kr>
2021-11-22 13:28:05 +0900

    genre_songs (노래 장르 선택, 게임 진행) 함수 작성 (미완성)

--- SingSingGame/game.py
+++ SingSingGame/game.py
@@ -7,19 +7,27 @@ class Game:
         self.user_name = []
         self.user_hint = []
 
+        self.kpop_singer = ["태연", "오마이걸", "에스파", "전소미", "스테이씨"]  # 가수
+        self.kpop_name = ["weekend", "dundundance", "nextlevel", "xoxo", "색안경"]  # 곡 제목
+        self.kpop_song = ["가장 가까운 바다. 혼자만의 영화관", "말리지 마 지금 내 기분은 feel so high", "위협에 맞서서 제껴라 제껴라 제껴라", "반쪽 같은 소리 하지 마. 내 반쪽 이제 내 거야", "패션은 과감한 게 좋아 Like it uh huh"]  # 하이라이트
+
+        self.ballade_singer = ["임창정", "벤", "윤종신", "허각", "다비치"]  # 가수
+        self.ballade_name = ["소주한잔", "열애중", "좋니", "hello", "너에게 못했던 마지막 말은"]  # 곡 제목
+        self.ballade_song = ["여보세요 나야 거기 잘 지내니", "누구보다 뜨겁게 사랑해", "사랑을 시작할때 내가 얼마나 예쁜지 모르지", "그대는 내 사랑 그리운 내 사랑", "긴밤을 새우고 아직 너를 많이 사랑한다고"]  # 하이라이트
+
     # 1. 씽씽게임하기
     def set_user(self):
         # 인원 수 입력받기
-        while(True):
+        while True:
             self.user_num = int(input('1~6(최대) 게임을 진행할 인원수를 입력하세요 ex) 6 : '))
             if 0 < self.user_num and self.user_num <= 6:
                 # 인원 수대로 사용자명 입력받기
                 count = 0
-                while(True):
+                while True:
                     name = input("사용자명을 입력하세요 : ")
                     self.user_name.append(name)
                     count += 1
-                    if(self.user_num == count):
+                    if self.user_num == count:
                         print('감사합니다~ 모든 사용자의 이름이 정상적으로 입력되었습니다~')
                         break
 
@@ -35,8 +43,53 @@ class Game:
 
     def genre_songs(self):
         # 노래 장르 선택하기 (발라드, 대중가요)
-        choice_song = input("발라드 or 대중가요 중 게임을 진행할 노래 장르를 선택하세요 : ")
+        while True:
+            choice_song = input("대중가요 or 발라드 중 게임을 진행할 노래 장르를 선택하세요 : ")
+            if choice_song == "대중가요":
+                print("지금부터 SingSingGame - 대중가요(k-pop) 버전이 시작됩니다!")
+                print('-' * 50)
+                while True:
+                    # kpop_list = random.sample(self.kpop_song, 1)
+                    # print(kpop_list)
+                    for i in self.kpop_song:
+                        for j in self.kpop_singer:
+                            for k in self.kpop_name:
+                                print(f'<다음 노래 가사를 보고 가수와 곡 제목을 쓰세요>\n {i}')
+                                user_singer_answer = input("가수 : ")
+                                user_song_name = input("곡 제목 : ")
+                                if user_singer_answer == j and user_song_name == k:
+                                    print("축하합니다!! 정답을 맞추셨습니다. ")
+                                    break
+                                elif user_singer_answer == j or user_song_name == k:
+                                    print("안타깝게 정답을 틀리셨습니다ㅠㅠ")
+                                    break
+                            break
+                        break
 
+            elif choice_song == "발라드":
+                print("지금부터 SingSingGame - 발라드 버전이 시작됩니다!")
+                print('-' * 50)
+                while True:
+                    # kpop_list = random.sample(self.kpop_song, 1)
+                    # print(kpop_list)
+                    for i in self.ballade_song:
+                        for j in self.ballade_singer:
+                            for k in self.ballade_name:
+                                print(f'<다음 노래 가사를 보고 가수와 곡 제목을 쓰세요>\n {i}')
+                                user_singer_answer = input("가수 : ")
+                                user_song_name = input("곡 제목 : ")
+                                if user_singer_answer == j and user_song_name == k:
+                                    print("축하합니다!! 정답을 맞추셨습니다. ")
+                                    break
+                                elif user_singer_answer == j or user_song_name == k:
+                                    print("안타깝게 정답을 틀리셨습니다ㅠㅠ")
+                                    break
+                            break
+                        break
+
+            else:
+                print("** 발라드와 대중가요 중 선택하세요 **")
+                continue
 
     def quiz(self):
         pass
@@ -53,5 +106,6 @@ class Game:
         pass
 
 game = Game()
-game.set_user()
+# game.set_user()
+game.genre_songs()
 # game.turn_user()
